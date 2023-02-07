a11ed0395b9af1f991cb6774cbf5e7488c754ad3
muxiaoru <1341995536@qq.com>
2023-02-07 16:28:56 +0800

    HomePage style change add a picture

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,26 +1,29 @@
 <template>
   <div class="container">
-    <router-link to="/form">表单</router-link>
-    <router-link to="/array">数组</router-link>
-    <router-link to="/exchange">汇率</router-link>
-    <router-link to="/movie">电影院座位</router-link>
-    <router-link to="/video">视频</router-link>
-    <router-link to="/foo">侧边栏和弹框</router-link>
-    <router-link to="/hangman">hangman game</router-link>
-    <router-link to="/mealfinder">meal finder</router-link>
-    <router-link to="/expensetracker">expense-tracker</router-link>
-    <router-link to="/music-player">music-player</router-link>
-    <router-link to="/infinite-scrolling">Infinite Scrolling</router-link>
-    <router-link to="/typing-game">Type Game</router-link>
-    <router-link to="/speech-text-reader">Speech TextReader</router-link>
-    <router-link to="/memory-cards">Memory Cards</router-link>
-    <router-link to="/cards-test">CardsTest</router-link>
-    <router-link to="/lyrics-search-app">LyricsSearchApp</router-link>
-    <router-link to="/relaxer-app">RelaxerApp</router-link>
-    <router-link to="/breakout-game">BreakoutGame</router-link>
-    <router-link to="/new-year-count-down">NewYearCountDown</router-link>
-    <router-link to="/sortable-list">SortableList</router-link>
-    <router-link to="/guess-number">GuessNumber</router-link>
+    <h1>Vue Projects</h1>
+    <p>List of projects that I am proud of</p>
+    <div class="projects-container">
+      <router-link to="/hangman" class="text-container"><iconify-icon icon="ph:game-controller" class="text-icon"></iconify-icon>Hangman Game</router-link>
+      <router-link to="/typing-game" class="text-container"><iconify-icon icon="material-symbols:match-word-rounded" class="text-icon"></iconify-icon>Typing Game</router-link>
+      <router-link to="/breakout-game" class="text-container"><iconify-icon icon="fluent:breakout-room-20-regular" class="text-icon"></iconify-icon>Breakout Game</router-link>
+      <router-link to="/form" class="text-container"><iconify-icon icon="fluent:form-20-regular" class="text-icon"></iconify-icon>Form Validator</router-link>
+      <router-link to="/array" class="text-container"><iconify-icon icon="material-symbols:data-array" class="text-icon"></iconify-icon>DOM Array Methods Project</router-link>
+      <router-link to="/exchange" class="text-container"><iconify-icon icon="icon-park-twotone:water-rate" class="text-icon"></iconify-icon>Exchange Rate Calculator</router-link>
+      <router-link to="/movie" class="text-container"><iconify-icon icon="mdi:movie-open-check-outline" class="text-icon"></iconify-icon>Movie Seat Booking</router-link>
+      <router-link to="/video" class="text-container"><iconify-icon icon="material-symbols:video-camera-back-rounded" class="text-icon"></iconify-icon>Custom Video Player</router-link>
+      <router-link to="/foo" class="text-container"><iconify-icon icon="vaadin:modal-list" class="text-icon"></iconify-icon>Menu Slider & Modal</router-link>
+      <router-link to="/mealfinder" class="text-container"><iconify-icon icon="game-icons:hot-meal" class="text-icon"></iconify-icon>Mealfinder App</router-link>
+      <router-link to="/expensetracker" class="text-container"><iconify-icon icon="arcticons:expense-register" class="text-icon"></iconify-icon>Expense Tracker</router-link>
+      <router-link to="/music-player" class="text-container"><iconify-icon icon="material-symbols:music-note" class="text-icon"></iconify-icon>Music Player</router-link>
+      <router-link to="/infinite-scrolling" class="text-container"><iconify-icon icon="mdi:scroll-outline" class="text-icon"></iconify-icon>Infinite Scrolling</router-link>
+      <router-link to="/speech-text-reader" class="text-container"><iconify-icon icon="material-symbols:speech-to-text" class="text-icon"></iconify-icon>Speech Text Reader</router-link>
+      <router-link to="/memory-cards" class="text-container"><iconify-icon icon="mdi:credit-cards" class="text-icon"></iconify-icon>Memory Cards</router-link>
+      <router-link to="/lyrics-search-app" class="text-container"><iconify-icon icon="material-symbols:lyrics-outline" class="text-icon"></iconify-icon>LyricsSearch App</router-link>
+      <router-link to="/relaxer-app" class="text-container"><iconify-icon icon="quill:breather" class="text-icon"></iconify-icon>Relaxer App</router-link>
+      <router-link to="/new-year-count-down" class="text-container"><iconify-icon icon="pepicons-pop:countdown" class="text-icon"></iconify-icon>New Year Countdown</router-link>
+      <router-link to="/sortable-list" class="text-container"><iconify-icon icon="mdi:sort" class="text-icon"></iconify-icon>Sortable List</router-link>
+      <router-link to="/guess-number" class="text-container"><iconify-icon icon="mdi:number-0-box-multiple-outline" class="text-icon"></iconify-icon>Speak Number Guessing Game</router-link>
+    </div>
   </div>
 </template>
 
@@ -29,9 +32,32 @@ export default {};
 </script>
 
 <style lang="css" scoped>
-.container {
+.container{
+  background: url("@/assets/img/briefbg.jpeg") center center/cover rgba(0, 0, 0, .5);
+  min-width: 100vw;
+  min-height: 100vh;
+  padding: 50px;
+}
+p{
+  color: #a6a3a3;
+}
+.projects-container {
   display: grid;
-  grid-template-columns: repeat(6, 1fr);
-  gap: 10px;
+  grid-template-columns: repeat(2, 1fr);
+  align-items: center;
+  justify-items: center;
+  font-size: 18px;
+}
+.text-container{
+  border: 1px solid #ddd;
+  text-align: left;
+  width: 100%;
+  padding: 10px 30px;
+  display: flex;
+  align-items: center;
+  color: #000000;
+}
+.text-icon{
+  padding: 0 10px;
 }
 </style>
